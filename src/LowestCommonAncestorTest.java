e94e2fdf8aead696ea0b91d422289dae19057baa
cjaenick <callum.janicke@gmail.com>
2019-10-02 20:49:18 +0200

    First Tests

--- src/LowestCommonAncestorTest.java
+++ src/LowestCommonAncestorTest.java
@@ -1,24 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class LowestCommonAncestorTest {
+public class LowestCommonAncestorTest {
 
 	@Test
-	void test() {
-		LowestCommonAncestor tree = new LowestCommonAncestor(); 
+	public void testFindLCA() {
+		
+		LowestCommonAncestor tree = new LowestCommonAncestor();
 		tree.root = new Node(1); 
-		tree.root.left = new Node(2); 
-		tree.root.right = new Node(3); 
-		tree.root.left.left = new Node(4); 
-		tree.root.left.right = new Node(5); 
-		tree.root.right.left = new Node(6); 
-		tree.root.right.right = new Node(7); 
-
-		System.out.println("LCA(4, 5): " + tree.findLCA(4,5)); 
-		System.out.println("LCA(4, 6): " + tree.findLCA(4,6)); 
-		System.out.println("LCA(3, 4): " + tree.findLCA(3,4)); 
-		System.out.println("LCA(2, 4): " + tree.findLCA(2,4));
+        tree.root.left = new Node(2); 
+        tree.root.right = new Node(3); 
+        tree.root.left.left = new Node(4); 
+        tree.root.left.right = new Node(5); 
+        tree.root.right.left = new Node(6); 
+        tree.root.right.right = new Node(7); 
+        assertEquals(tree.findLCA(2, 4),2);
+        assertEquals(tree.findLCA(6, 7),3);
+        assertEquals(tree.findLCA(5, 5),5);
+        
+	
 	}
-
 }
